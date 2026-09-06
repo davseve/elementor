c4fd484e3e03fca559e177a80b387d63fb5ada40
David Seveloff <davids@elementor.com>
2026-09-06 11:23:48 +0300

    re-add comments

--- core/utils/svg/svg-sanitizer.php
+++ core/utils/svg/svg-sanitizer.php
@@ -728,15 +728,20 @@ class Svg_Sanitizer {
 	 */
 	private function sanitize_elements() {
 		$elements = $this->svg_dom->getElementsByTagName( '*' );
+		// loop through all elements
+		// we do this backwards so we don't skip anything if we delete a node
+		// see comments at: http://php.net/manual/en/class.domnamednodemap.php
 		for ( $index = $elements->length - 1; $index >= 0; $index-- ) {
 			/**
 			 * @var \DOMElement $current_element
 			 */
 			$current_element = $elements->item( $index );
+			// If the tag isn't in the whitelist, remove it and continue with next iteration
 			if ( ! $this->is_allowed_tag( $current_element ) ) {
 				continue;
 			}
 
+			// validate element attributes
 			$this->validate_allowed_attributes( $current_element );
 
 			$this->strip_xlinks( $current_element );
@@ -744,8 +749,6 @@ class Svg_Sanitizer {
 			if ( 'use' === strtolower( $current_element->tagName ) ) { // phpcs:ignore -- php DomDocument
 				$this->validate_use_tag( $current_element );
 			}
-
-			$this->sanitize_element_child_nodes( $current_element );
 		}
 	}
 
@@ -801,32 +804,5 @@ class Svg_Sanitizer {
 	private function strip_line_breaks( $content ) {
 		// Remove line breaks.
 		return preg_replace( '/\r|\n/', '', $content );
-	}
-
-	private function sanitize_element_child_nodes( \DOMElement $element ) {
-		$child_nodes = iterator_to_array( $element->childNodes );
-
-		foreach ( $child_nodes as $child ) {
-			if ( XML_TEXT_NODE === $child->nodeType ) {
-				$child->nodeValue = $this->sanitize_text_node_value( $child->nodeValue );
-				continue;
-			}
-
-			if ( XML_CDATA_SECTION_NODE === $child->nodeType ) {
-				$element->removeChild( $child );
-				continue;
-			}
-		}
-	}
-
-	private function sanitize_text_node_value( $value ) {
-		if ( '' === $value ) {
-			return $value;
-		}
-		// CDATA markers must not survive into inlined HTML.
-		$value = str_replace( [ '<![CDATA[', ']]>' ], '', $value );
-		// No angle brackets — they become live markup in an HTML parser.
-		$value = str_replace( [ '<', '>' ], '', $value );
-		return $value;
 	}
 }
